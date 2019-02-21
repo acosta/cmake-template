23339cf270e7123175daede13bd9964424b7ccd2
Afonso R. Costa Jr <arabelo@gmail.com>
2019-02-20 21:16:11 -0400

    Add support for Boost library
    It is just an example how to use a system library using find_package.
    
    Fix #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cmath>
+#include <boost/timer/timer.hpp>
 
 int main()
 {
+  boost::timer::auto_cpu_timer t;
+
   std::cout << "Hello CMake Template!" << std::endl;
+
+  for (long i = 0; i < 100000000; ++i) {
+    std::sqrt(123.456L);
+  }
+
   return 0;
 }
